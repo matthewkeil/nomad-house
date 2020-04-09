324647ac2ab628a6fab74bacbbaaeecf985351da
Chad Kaplan <chadkaplan@knights.ucf.edu>
2020-04-09 16:30:56 -0400

    Adding the LearningPage component

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+    <v-content>
+    <LearningPage>
+    </LearningPage>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import LearningPage from './components/LearningPage';
+export default {
+  name: 'App',
+
+  components: {
+    LearningPage,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/components/LearningPage.vue
@@ -0,0 +1,41 @@
+<template>
+<v-app>
+    <v-content>
+        <v-card>
+            <v-img class="mx-auto" max-height="500px" :src="require('../assets/Logo.png')" />
+            <v-card-title>
+                Title
+            </v-card-title>
+            <v-card-subtitle>
+                Subtitle: Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem delectus modi dolor facilis sed minus a nesciunt, nulla, nobis numquam, consectetur eveniet? Omnis, quia vel suscipit cumque error sed eligendi?
+            </v-card-subtitle>
+<v-tabs
+      v-model="active"
+      color="cyan"
+      dark
+      slider-color="yellow"
+    >
+      <v-tab>
+          Graphs
+      </v-tab>
+      <v-tab>
+          Graphs
+      </v-tab>
+      <v-tab>
+          Graphs
+      </v-tab>
+    </v-tabs>
+        </v-card>
+    </v-content>
+</v-app>
+</template>
+
+<script>
+export default {
+  name: 'LearningPage'
+}
+</script>
+
+<style>
+
+</style>
